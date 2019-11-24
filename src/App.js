654a6004e57ae24d178a2dbc016457c78a2a985a
PRABHAKAR9107 <02panjiyara@gmail.com>
2019-11-24 16:46:07 -0500

    react router

--- src/App.js
+++ src/App.js
@@ -2,8 +2,31 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Home from "./pages/Home";
+import Recipe from "./pages/Recipes";
+
+import SingleRecipe from "./pages/SingleRecipe";
+import Default from "./pages/Default";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <Router>
+      <main>
+        {/* navbar */}
+
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/recipes" component={Recipe} />
+
+          <Route component={Default} />
+        </Switch>
+      </main>
+    </Router>
+  );
 }
 
 export default App;
