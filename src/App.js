9b0be35925d1957de730284de705db72531ad48c
PRABHAKAR9107 <02panjiyara@gmail.com>
2019-11-26 03:50:48 -0500

    router-set-up

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Home from "./pages/Home";
-import Recipe from "./pages/Recipes";
-
+import Recipes from "./pages/Recipes";
 import SingleRecipe from "./pages/SingleRecipe";
 import Default from "./pages/Default";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <main>
+          {/* navbar */}
 
-function App() {
-  return (
-    <Router>
-      <main>
-        {/* navbar */}
-
-        <Switch>
-          <Route path="/" component={Home} />
-          <Route path="/recipes" component={Recipe} />
-
-          <Route component={Default} />
-        </Switch>
-      </main>
-    </Router>
-  );
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/recipes" exact component={Recipes} />
+            <Route path="/recipes/:id" component={SingleRecipe} />
+            <Route component={Default} />
+          </Switch>
+        </main>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,6 @@ import React, { Component } from "react";
 
 export default class Home extends Component {
   render() {
-    return <h4></h4>;
+    return <h4>Home</h4>;
   }
 }
